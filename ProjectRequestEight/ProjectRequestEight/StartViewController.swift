c837999682572df51c343dac9aa4b6c148bedd5a
Aninha <aninhapss2000@gmail.com>
2023-03-20 21:34:52 -0300

    Added button, label startViewController

--- ProjectRequestEight/ProjectRequestEight/StartViewController.swift
+++ ProjectRequestEight/ProjectRequestEight/StartViewController.swift
@@ -9,21 +9,16 @@ import UIKit
 
 class StartViewController: UIViewController {
 
+    @IBOutlet var start: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func start(_ sender: Any) {
+        if let tableView = self.storyboard?.instantiateViewController(identifier: "table") {
+            start.backgroundColor = .orange
+            self.present(tableView, animated: true)
+        }
     }
-    */
-
 }
