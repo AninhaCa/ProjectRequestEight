8d31aedce09016ea9de265a72fc287a6e45fd562
Aninha <aninhapss2000@gmail.com>
2023-03-20 21:01:53 -0300

    Added ProjectRequest

---
+++ ProjectRequestEight/ProjectRequestEight/MyCustomXIBCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyCustomXIBCell.swift
+//  ProjectRequestEight
+//
+//  Created by Ana Paula Silva de Sousa on 20/03/23.
+//
+
+import UIKit
+
+class MyCustomXIBCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ ProjectRequestEight/ProjectRequestEight/Pizza.swift
@@ -0,0 +1,8 @@
+//
+//  Pizza.swift
+//  ProjectRequestEight
+//
+//  Created by Ana Paula Silva de Sousa on 20/03/23.
+//
+
+import Foundation
